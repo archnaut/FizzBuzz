64fca64f5a2b949e386a4bcf71bf74400b818533
Paul Clements <paulcclements@gmail.com>
2015-08-23 15:58:07 -0500

    Added multiple asserts to tests

--- Tests.Unit/WhenTransformingValue.cs
+++ Tests.Unit/WhenTransformingValue.cs
@@ -19,24 +19,33 @@ namespace Tests.Unit
         public void GivenValueDivisibleByNeitherThreeOrFive()
         {
             Assert.AreEqual("1", _fizzBuzzService.Transform(1));
+            Assert.AreEqual("2", _fizzBuzzService.Transform(2));
+            Assert.AreEqual("7", _fizzBuzzService.Transform(7));
         }
 
         [TestMethod]
         public void GivenValueDivisibleByThreeAndNotDivisibleByFive()
         {
             Assert.AreEqual("Fizz", _fizzBuzzService.Transform(3));
+            Assert.AreEqual("Fizz", _fizzBuzzService.Transform(6));
+            Assert.AreEqual("Fizz", _fizzBuzzService.Transform(9));
+
         }
 
         [TestMethod]
         public void GivenValueDivisibleByFiveAndNotDivisibleByThree()
         {
             Assert.AreEqual("Buzz", _fizzBuzzService.Transform(5));
+            Assert.AreEqual("Buzz", _fizzBuzzService.Transform(10));
+            Assert.AreEqual("Buzz", _fizzBuzzService.Transform(20));
         }
         
         [TestMethod]
         public void GivenValueDivisibleByThreeAndFive()
         {
             Assert.AreEqual("FizzBuzz", _fizzBuzzService.Transform(15));
+            Assert.AreEqual("FizzBuzz", _fizzBuzzService.Transform(30));
+            Assert.AreEqual("FizzBuzz", _fizzBuzzService.Transform(45));
         }
     }
 }
