3b01a6ccaf496fd44d4f58412b6db997e99a7a5c
Paul Clements <paulcclements@gmail.com>
2015-09-25 21:18:45 -0500

    Added Two Tests

---
+++ Tests.Unit/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FizzBuzz;
+
+namespace Tests.Unit
+{
+    [TestClass]
+    public class MiscTests
+    {
+        [TestMethod]
+        public void FizzSpecification_Is_A_ISpecification()
+        {
+            Assert.IsInstanceOfType(new FizzSpecification(), typeof(ISpecification<int>));
+        }
+
+        [TestMethod]
+        public void BuzzSpecification_Is_A_ISpecification()
+        {
+            Assert.IsInstanceOfType(new BuzzSpecification(), typeof(ISpecification<int>));
+        }
+    }
+}
